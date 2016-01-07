2cf89fd34a22657ba602b891c1554314a3464e07
Ken Rimple <krimple@chariotsolutions.com>
2016-01-07 09:29:32 -0500

    removed .js and .js.map files from src and added to .gitignore

--- src/app/app.js
+++
@@ -1,6 +0,0 @@
-var app_shell_1 = require('./app-shell/app-shell');
-var browser_1 = require("angular2/platform/browser");
-var http_1 = require("angular2/http");
-var router_1 = require('angular2/router');
-browser_1.bootstrap(app_shell_1.AppShell, [http_1.HTTP_PROVIDERS, router_1.ROUTER_PROVIDERS]);
-//# sourceMappingURL=app.js.map
--- src/app/bootstrap/components/alert/alert-message.js
+++
@@ -1,8 +0,0 @@
-var AlertMessage = (function () {
-    function AlertMessage(text) {
-        this.text = text;
-    }
-    return AlertMessage;
-})();
-exports.AlertMessage = AlertMessage;
-//# sourceMappingURL=alert-message.js.map
